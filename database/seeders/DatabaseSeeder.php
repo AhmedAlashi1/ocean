600740691b501ac1526fa35f0a69b30b7f7490fa
AhmedAlashi1 <aalshy00@gmail.com>
2025-03-15 11:33:20 +0200

    update web

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,17 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-//            AdminSeeder::class,
-//            ClientSatisfactionsSeeder::class,
-//            CompaniesSeeder::class,
-//            QuestionsSeeder::class,
-//            ServicesSeeder::class,
-//            TermSeeder::class,
-//            WorksSeeder::class,
-//            ProcessSeeder::class,
-//            BlogTypeSeeder::class,
-//            MemberSeeder::class,
-//            SettingSeeder::class,
+            AdminSeeder::class,
+            ClientSatisfactionsSeeder::class,
+            CompaniesSeeder::class,
+            QuestionsSeeder::class,
+            ServicesSeeder::class,
+            TermSeeder::class,
+            WorksSeeder::class,
+            ProcessSeeder::class,
+            BlogTypeSeeder::class,
+            MemberSeeder::class,
+            SettingSeeder::class,
             SettingSeoSeeder::class,
 
         ]);
